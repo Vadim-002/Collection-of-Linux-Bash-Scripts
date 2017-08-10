c3bf1734a6df2cf35b68f93a8fd4d9fec3ede25d
xiaoshuaigege <quliang.8605@163.com>
2017-08-10 19:54:52 +0800

    重新优化了reset.sh

--- 系统初始化/README.md
+++ 系统初始化/README.md
@@ -1,7 +1,28 @@
 # Collection-of-Linux-Bash-Scripts
 
 
-# 系统初始化
+# 系统初始化  
+
+系统初始化，一共具备如下功能
+
+- 判断是否是root用户在执行此操作
+- 添加了 -v|--verbose 以及-h |--help 两个选项
+- 关闭防火墙
+- 关闭SElinux
+- 判断系统版本
+- 根据系统的版本执行不同的操作
+- 配置VIM的默认设置
+- 添加默认用户
+- 初始化 bash初始化脚本 initBashSctipt.sh
+- 配置yum源
+- 安装常用软件 tree vsftpd telnet lftp
+
+
+
+## 有待于优化的部分内容
+- 配置默认的环境变量
+- 添加常用的别名
+- 配置网络环境，例如自动获取IP，或者修改为静态IP
 
 
 
--- 系统初始化/reset.sh
+++ 系统初始化/reset.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 #-----------------------------
-#Filename: Hello
-#Version: 1.0
-#Date: 2017-08-03 22:05:27
+#Filename: reset.sh
+#Version: 0.5
+#Date: 2017-08-10 15:05:27
 #Author: xiaoshuaigege
 #Website:www.pojun.tech
 #Description: This is a OS init  bash script
@@ -11,24 +10,66 @@
 #License: GPL
 SUCCESS=0
 ERROR=100
+ROOT_ERROR=101
+VERBOSE=0
 
 
+#先判断当前用户，只有root用户能够执行这个脚本
+
+if [[ "$(whoami)" != 'root'  ]]; then 
+    echo "Only root can run this script!!"
+    exit $ROOT_ERROR
+fi
+
+#添加help 选项和 verbose 选项
+if [ $# -eq 1 ]; then
+case $1 in
+-v|--verbose)
+    VERBOSE=1
+    ;;
+-h|--help)
+    echo "Usage: `basename $0`  -v|--verbose|-h|--help"
+    exit $SUCCESS
+    ;;
+esac
+fi
 
 echo -e "\033[36m
 +--------------------------------------------------------------+
 |         === Welcome to Centos System init ===                |
 +--------------------------------------------------------------+
 \033[0m"
+#关闭SELinux
+closeSELinux(){
+    sed 's/^SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config 
+    [ $VERBOSE -eq 1 ] && echo "SELinux is closed"
+}
+#CentOS 7中的特殊操作
+centosInit7(){
+    #关闭防火墙
+    systemctl disable firewalld.service
+    systemctl stop firewalld.service
+    [ $VERBOSE -eq 1 ] && echo "firewalld  is closed"
+}
+#CentOS 6中的特殊操作
+centosInit6(){
+    #关闭防火墙
+    chkconfig iptables off
+    service iptables stop
+    [ $VERBOSE -eq 1 ] && echo "firewalld  is closed"
+}
+
+
 # 判断系统版本
 OS_RELEASE=`grep -o " [0-9]" /etc/redhat-release|cut -d" " -f2`
 if [ "$OS_RELEASE" -eq 7 ];then
 	echo   "System Release:CentOS 7 \n   "
 #用来执行CentOS7 中特有的 功能
-#	bash centos7_init.sh
+	centosInit7
 else [ "$OS_RELEASE" -eq 6 ];
 	echo   "System Release:CentOS 6 \n  "
 #用来执行CentOS6中与CentOS7不一样的地方
-#	bash centos6_init.sh
+	centosInit6
 fi
 
 #添加默认用户
@@ -36,7 +77,7 @@ fi
 id mage &>/dev/null
 #$?的作用是引用本条命令的执行结果
 if [ $?  -eq 0  ]; then
-	echo "the uesr mage has already exits "
+    [ $VERBOSE -eq 1 ] && echo "the uesr mage has already exits"
 else
 	useradd mage
 	if [ $?  -eq 0 ]; then
@@ -125,16 +166,14 @@ if [ ! -f "$yumRepo" ]; then
 fi  
 
 if [  $ifYumRepoExits -eq 0  ]; then 
-	cat >> /etc/vimrc <<EOF
+	cat >> $yumRepo <<EOF
 		
 		[yum]
 		name=mage centos yum 
 		baseurl=http://172.16.0.1/centos/$releasever/
 		gpgcheck=0
 
-
-
-		[epel]
+        [epel]
 		name=mage epel yum
 		baseurl=http://172.16.0.1/fedora-epel/$releasever/
 		gpgcheck=0
@@ -145,7 +184,7 @@ fi
 
 
 # 判断是否安装了某个软件包
-
+installTree(){
 rpm -q tree
 
 if [ ! $? -eq 0  ]; then 
@@ -154,8 +193,9 @@ if [ ! $? -eq 0  ]; then
 	
 	[  $? -eq 0  ] && echo "tree has installed"
 fi
-
+}
 # 安装ftp 包
+installFtp(){
 rpm -q vsftpd
 
 if [ ! $? -eq 0  ]; then 
@@ -165,12 +205,12 @@ if [ ! $? -eq 0  ]; then
 	systemctl start vsftpd 
 	#设为开机自动启动ftp服务
 	systemctl enable vsftpd 
-	
-	
 
 fi
+}
 
 # 安装telnet 包
+installTelnet(){
 rpm -q telnet
 
 if [ ! $? -eq 0  ]; then 
@@ -178,8 +218,11 @@ if [ ! $? -eq 0  ]; then
 	yum install telnet
 	[  $? -eq 0  ] && echo "telnet has installed"
 fi
+}
+
 
 # 安装lftp 包
+installLftp(){
 rpm -q lftp
 
 if [ ! $? -eq 0  ]; then 
@@ -187,5 +230,15 @@ if [ ! $? -eq 0  ]; then
 	yum install lftp
 	[  $? -eq 0  ] && echo "lftp has installed"
 fi
+}
+
+installTree
+installFtp
+installTelnet
+installLftp
+
+
+
+
 
 exit $SUCCESS
