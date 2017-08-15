4f9a9fb388b26cd96de25e450b2adedf3fd0e69e
xiaoshuaigege <quliang.8605@163.com>
2017-08-15 11:59:14 +0800

    优化了部分代码，并经过了校验

--- 系统初始化/reset.sh
+++ 系统初始化/reset.sh
@@ -2,7 +2,7 @@
 #-----------------------------
 #Filename: reset.sh
 #Version: 0.5
-#Date: 2017-08-10 15:05:27
+#Date: 2017-08-15 15:05:27
 #Author: xiaoshuaigege
 #Website:www.pojun.tech
 #Description: This is a OS init  bash script
@@ -21,73 +21,162 @@ if [[ "$(whoami)" != 'root'  ]]; then
     exit $ROOT_ERROR
 fi
 
-#添加help 选项和 verbose 选项
-if [ $# -eq 1 ]; then
-case $1 in
--v|--verbose)
-    VERBOSE=1
-    ;;
--h|--help)
-    echo "Usage: `basename $0`  -v|--verbose|-h|--help"
-    exit $SUCCESS
-    ;;
-esac
+echoUsage(){
+    echo -e "
+Usage:
+     -v|--verbose    see what is being done
+     -h|--help       display this play text and exit
+     -d|--default    to initialize the system by default
+    "
+}
+# 如果用户默认不加任何选项，将提示默认安装
+if [ $# -eq 0  ]; then
+    read -p "The system is going to be initialized by default [y/n]?" CHOOSE
+    #如果选择n则提示脚本的使用方法
+    [ "$CHOOSE" == "N"  ] || [ "$CHOOSE" == "n" ] && {
+        echoUsage
+		exit $SUCCESS
+    }
+    #选择y则进行默认安装
+else
+    #判断help选项和verbose选项
+    for I in 'seq $#'; do
+    case $1 in
+    -v|--verbose)
+        VERBOSE=1
+        shift 1
+        ;;
+    -h|--help)
+        echo -e "Name: `basename $0` -init the mini Linux"
+        echoUsage
+        exit $SUCCESS
+        ;;
+    -d|--default)
+        # 目前默认只支持默认安装，以后会加入更多的内容
+        # 如果不输入-d 选项的话，会提示是否默认安装
+        ;;
+    esac
+    done
 fi
 
-echo -e "\033[36m
-+--------------------------------------------------------------+
-|         === Welcome to Centos System init ===                |
-+--------------------------------------------------------------+
-\033[0m"
-#关闭SELinux
-closeSELinux(){
-    sed 's/^SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config 
-    [ $VERBOSE -eq 1 ] && echo "SELinux is closed"
-}
-#CentOS 7中的特殊操作
-centosInit7(){
+#提示系统即将被初始化
+[ $VERBOSE -eq 1 ] && echo "The system is going to be initialized"
+
+#CentOS 7 中的特殊操作
+centos7init(){
     #关闭防火墙
     systemctl disable firewalld.service
     systemctl stop firewalld.service
-    [ $VERBOSE -eq 1 ] && echo "firewalld  is closed"
+    [ $VERBOSE -eq 1 ] && echo "firewalld is closed"
+	
+	
+	#初始化网络环境
+	[ $VERBOSE -eq 1 ] && echo "restart network"
+	sed -i 's/ONBOOT-NO/ONBOOT=yes/' ifcfg-enss33
+	service network restart
 }
 #CentOS 6中的特殊操作
-centosInit6(){
+centos6init(){
     #关闭防火墙
     chkconfig iptables off
     service iptables stop
-    [ $VERBOSE -eq 1 ] && echo "firewalld  is closed"
+    [ $VERBOSE -eq 1] && echo "iptables is closed"
 }
 
+#关闭SELinux
+closeSELinux(){
+    sed -n 's/^SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+    [ $VERBOSE -eq 1 ] && echo "SELinux is closed"
+}
 
-# 判断系统版本
+#判断系统版本
 OS_RELEASE=`grep -o " [0-9]" /etc/redhat-release|cut -d" " -f2`
-if [ "$OS_RELEASE" -eq 7 ];then
-	echo   "System Release:CentOS 7 \n   "
-#用来执行CentOS7 中特有的 功能
-	centosInit7
-else [ "$OS_RELEASE" -eq 6 ];
-	echo   "System Release:CentOS 6 \n  "
-#用来执行CentOS6中与CentOS7不一样的地方
-	centosInit6
+if [ "$OS_RELEASE" -eq 7 ]; then
+    centos7init
+elif [ "$OS_RELEASE" -eq 6 ]; then
+    centos6init
 fi
 
+#调用
+closeSELinux
+
 #添加默认用户
-#首先判断是否有相应的用户
-id mage &>/dev/null
-#$?的作用是引用本条命令的执行结果
+#首先判断是否已经有了默认的用户
+id mage &> /dev/null
+
 if [ $?  -eq 0  ]; then
     [ $VERBOSE -eq 1 ] && echo "the uesr mage has already exits"
 else
-	useradd mage
-	if [ $?  -eq 0 ]; then
-		echo 123456|passwd --stdin mage &> /dev/null
-	else 
-		echo " add user mage error  "
-	fi
+    useradd mage
+    if [ $?  -eq 0 ]; then
+        echo 123456|passwd --stdin mage &> /dev/null
+        [ $VERBOSE -eq 1 ] && echo "add user mage succeed"
+    else
+        [ $VERBOSE -eq 1 ] && echo "add user mage error"
+    fi
 fi
 
 
+#配置yum源和epel源
+YUM_REPO=/etc/yum.repos.d/sysinit.repo
+#0表示文件不存在 1表示文件已经存在
+YUM_REPO_EXITS=1
+if [ ! -f  "$YUM_REPO" ]; then 
+    #文件不存在
+    YUM_REPO_EXITS=0
+    touch "$YUM_REPO"
+    [  $? -eq 0  ] && [ $VERBOSE -eq 1 ] && echo "touch file $YUM_REPO SUCCESS"
+fi
+
+# 向文件中追加yum源的配置内容
+if [  $YUM_REPO_EXITS -eq 0  ]; then
+
+	if [ "$OS_RELEASE" -eq 7 ]; then
+    cat >> $YUM_REPO << EOF
+
+[yum]
+name=mage centos yum
+baseurl=http://172.16.0.1/centos/7/
+gpgcheck=0
+
+[epel]
+name=mage epel yum
+baseurl=http://172.16.0.1/fedora-epel/7/x86_64/
+gpgcheck=0
+		
+EOF
+	elif [ "$OS_RELEASE" -eq 6 ]; then
+		cat >> $YUM_REPO << EOF
+
+[yum]
+name=mage centos yum
+baseurl=http://172.16.0.1/centos/6/
+gpgcheck=0
+
+[epel]
+name=mage epel yum
+baseurl=http://172.16.0.1/fedora-epel/6/x86_64/
+gpgcheck=0
+		
+EOF
+	fi
+    
+
+[ $VERBOSE -eq 1 ] && echo "yum repository has been established"
+
+fi
+
+
+# 判断是否安装了vim
+installVim(){
+rpm -q vim
+
+if [  $? -ne 0  ]; then 
+	# 安装 vim 包
+	yum install vim 
+	
+	[  $? -eq 0  ] &&  [ $VERBOSE -eq 1 ] && echo "vim has installed"
+	
 #配置vim的一些默认属性
 #静默模式判断该配置文件中是否已经定义过相关的属性，如果是，则不添加配置
 #如果没有添加过，则进行添加
@@ -102,105 +191,43 @@ grep -q "\<mage setting\>" /etc/vimrc ||{
 	syntax on
 EOF
 }
-
-#创建默认的脚本初始化文件 initBashScript.sh
-initFile=/bin/initBashScript.sh
-#0表示文件不存在 1表示文件已经存在
-ifInitFileExits=1;
-if [ ! -f "$initFile" ]; then  
-    #文件不存在
-    ifInitFileExits=0
-　　touch "$initFile"  
-    chmod +x $initFile
-fi  
-
-if [  $ifInitFileExits -eq 0  ]; then 
-
-	cat >>$initFile<<END
-	'#!/bin/bash
-	[ $# -gt 1 ] && echo "Arg only one" && exit 100
-	[ $# -eq 0 ] && read -p "Please input scriptname: " filename
-	#判断文件个数
-	[ $# -eq 1 ] && filename=$1
-
-	#判断文件是否存在
-	[ -a "$filename" ] && echo $filename is exist && exit 101
-	#创建文件
-	touch $filename
-	#加上可执行权限
-	chmod +x $filename
-
-	cat > $filename << EOF
-	#!/bin/bash
-	#-----------------------------
-	#Filename: $filename
-	#Version: 1.0
-	#Date: `date"+%F %T"`
-	#Author: mage
-	#Website:www.pojun.tech
-	#Description: This is a bash script
-	#-----------------------------
-	#License: GPL
-
-	EOF
-
-	vim +  $filename'
-	
-END
-
-else 
-	echo "touch file initBashScript.sh failed"
-	
 fi
+}
 
 
+# 判断是否安装了ssh
+installSSH(){
+rpm -q ssh
 
-#配置yum源和epel源
-yumRepo=/etc/yum.repos.d/CentOS-Mage.repo
-#0表示文件不存在 1表示文件已经存在
-ifYumRepoExits=1;
-if [ ! -f "$yumRepo" ]; then  
-    #文件不存在
-    ifYumRepoExits=0
-　　touch "$yumRepo"  
-fi  
-
-if [  $ifYumRepoExits -eq 0  ]; then 
-	cat >> $yumRepo <<EOF
-		
-		[yum]
-		name=mage centos yum 
-		baseurl=http://172.16.0.1/centos/$releasever/
-		gpgcheck=0
-
-        [epel]
-		name=mage epel yum
-		baseurl=http://172.16.0.1/fedora-epel/$releasever/
-		gpgcheck=0
-
-EOF
-
+if [ ! $? -eq 0  ]; then 
+	# 安装 ssh 包
+	yum install ssh 
+	
+	[  $? -eq 0  ] &&  [ $VERBOSE -eq 1 ] && echo "ssh has installed"
 fi
+}
 
 
-# 判断是否安装了某个软件包
+
+# 判断是否安装了Tree
 installTree(){
 rpm -q tree
 
-if [ ! $? -eq 0  ]; then 
+if [  $? -ne 0  ]; then 
 	# 安装 tree 包
 	yum install tree 
 	
-	[  $? -eq 0  ] && echo "tree has installed"
+	[  $? -eq 0  ] &&  [ $VERBOSE -eq 1 ]  && echo "tree has installed"
 fi
 }
 # 安装ftp 包
 installFtp(){
 rpm -q vsftpd
 
-if [ ! $? -eq 0  ]; then 
+if [ $? -ne 0  ]; then 
 	# 安装 ftp
 	yum install vsftpd 
+	[  $? -eq 0  ] &&  [ $VERBOSE -eq 1 ]  && echo "vsftpd has installed"
 	#启动ftp服务
 	systemctl start vsftpd 
 	#设为开机自动启动ftp服务
@@ -213,10 +240,10 @@ fi
 installTelnet(){
 rpm -q telnet
 
-if [ ! $? -eq 0  ]; then 
+if [ $? -ne 0  ]; then 
 	# 安装 telnet
 	yum install telnet
-	[  $? -eq 0  ] && echo "telnet has installed"
+	[  $? -eq 0  ]  &&  [ $VERBOSE -eq 1 ] && echo "telnet has installed"
 fi
 }
 
@@ -225,20 +252,36 @@ fi
 installLftp(){
 rpm -q lftp
 
-if [ ! $? -eq 0  ]; then 
+if [ $? -ne 0  ]; then 
 	# 安装 lftp
 	yum install lftp
-	[  $? -eq 0  ] && echo "lftp has installed"
+	[  $? -eq 0  ]  &&  [ $VERBOSE -eq 1 ] && echo "lftp has installed"
+fi
+}
+# 安装autofs
+installAutofs(){
+rpm -q autofs
+
+if [  $? -ne 0  ]; then 
+	# 安装 autofs
+	yum install autofs
+	[  $? -eq 0  ]  &&  [ $VERBOSE -eq 1 ] && echo "autofs has installed"
 fi
 }
 
+installVim
+installSSH
 installTree
 installFtp
 installTelnet
 installLftp
 
 
+installDevelopmentTools(){
 
+	yum  groupinstall Development tools
+
+}
 
 
 exit $SUCCESS
